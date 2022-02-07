8bd29c25b535fcc6dcd3dbadcbf42d5370325146
WangDanpeng <wangdanpeng0@gmail.com>
2022-02-07 17:10:42 +0800

    Site updated: 2022-02-07 17:10:42

--- jars/time.js
+++ jars/time.js
@@ -10,12 +10,12 @@ function LoadBlogParts(){
 	
 	var sHtml = "";
 	sHtml += '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="160" height="70" id="' + swfTitle + '" align="middle">';
-	sHtml += '<param name="allowScriptAccess" value="always">';
-	sHtml += '<param name="movie" value="' + sUrl + '">';
-	sHtml += '<param name="quality" value="high">';
-	sHtml += '<param name="bgcolor" value="#ffffff">';
-	sHtml += '<param name="wmode" value="transparent">';
-	sHtml += '<embed wmode="transparent" src="' + sUrl + '" quality="high" bgcolor="#ffffff" width="360" height="140" name="' + swfTitle + '" align="middle" allowscriptaccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer">';
+	sHtml += '<param name="allowScriptAccess" value="always" />';
+	sHtml += '<param name="movie" value="' + sUrl + '" />';
+	sHtml += '<param name="quality" value="high" />';
+	sHtml += '<param name="bgcolor" value="#ffffff" />';
+	sHtml += '<param name="wmode" value="transparent" />';
+	sHtml += '<embed wmode="transparent" src="' + sUrl + '" quality="high" bgcolor="#ffffff" width="360" height="140" name="' + swfTitle + '" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />';
 	sHtml += '</object>';
 	
 	document.write(sHtml);
